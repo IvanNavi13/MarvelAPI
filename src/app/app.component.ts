a5a761dd120f3ad3f55b3871c58381c64f3db10d
IvanNavi13 <ivannavi.13@hotmail.com>
2022-09-27 21:00:28 -0500

    estructura del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
+import { FindComponent } from './components/modules/character/pages/find/find.component';
+import { ListComponent } from './components/modules/character/pages/list/list.component';
+import { LoginComponent } from './components/modules/auth/pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    FindComponent,
+    ListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
